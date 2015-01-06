4c25045374cdc581a4bbb7d19f6efb57a9b5916a
Sergey Eremin <yeriomin@gmail.com>
2015-01-06 16:25:22 +0300

    Fixed result sorting and added cpu core get method

--- server.js
+++ server.js
@@ -38,21 +38,46 @@ server.get('/result/:id', function (req, res, next) {
 server.get('/result', function (req, res, next) {
     res.charSet('utf-8');
     var query = knex.select().from('results');
+    if (req.query.core) {
+        query.where('core', req.query.core);
+    }
     var limit = req.query.limit ? req.query.limit : 10;
     query.limit(limit);
     if (req.query.offset) {
         query.offset(req.query.offset);
     }
+    var sortColumn = 'id', sortDirection = 'DESC';
     if (req.query.sort) {
-        var direction = req.query.direction == 'ASC' ? req.query.direction : 'DESC';
-        query.orderBy(req.query.sort, direction);
+        sortColumn = req.query.sort;
     }
+    if (req.query.direction && req.query.direction == 'ASC') {
+        sortDirection = 'ASC';
+    }
+    query.orderBy(sortColumn, sortDirection);
     query.exec(function(err, rows) {
         if (err) {
             console.log(err);
             res.send(500, err);
         } else {
             res.send(rows);
+        }
+    });
+    return next();
+});
+
+server.get('/core', function (req, res, next) {
+    res.charSet('utf-8');
+    var query = knex.select('core').from('results').groupBy('core').orderBy('core', 'asc');
+    query.exec(function(err, rows) {
+        if (err) {
+            console.log(err);
+            res.send(500, err);
+        } else {
+            var cores = [];
+            for (var i in rows) {
+                cores.push(rows[i].core);
+            }
+            res.send(cores);
         }
     });
     return next();
